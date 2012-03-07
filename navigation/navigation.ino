180596d2e3b318a33aed9e0b7efecadb569b3d96
Taylor Perkins <taylorhp@gmail.com>
2012-03-07 00:24:42 -0500

    Tested distance function on Duemilanove. Working as intended.

--- navigation/navigation.ino
+++ navigation/navigation.ino
@@ -31,10 +31,10 @@ void loop()
 
 boolean checkSanity(){
   
-  double distanceSanity = 999;
-  //double distanceSanity = computeDistanceAndBearing(LATITUDES[0],LONGITUDES[0],LATITUDES[NUMBER_OF_WAYPOINTS],LONGITUDES[NUMBER_OF_WAYPOINTS]);
-  
-  Serial.write("3");
+  double distanceSanity = computeDistanceAndBearing(LATITUDES[0],LONGITUDES[0],LATITUDES[NUMBER_OF_WAYPOINTS-1],LONGITUDES[NUMBER_OF_WAYPOINTS-1]);
+ 
+  printDouble(distanceSanity,5);
+  Serial.write("\n");
   
   //boolean droneSanity = checkDroneSanity();
   
@@ -133,10 +133,42 @@ double computeDistanceAndBearing(double lat1, double lon1,
             }
         }
 
-      float distance = (float) (b * A * (sigma - deltaSigma));
+      double distance = (b * A * (sigma - deltaSigma));
       
-      return ((double)distance);
+      return (distance);
  }
+ 
+ void printDouble( double val, byte precision){
+  // prints val with number of decimal places determine by precision
+  // precision is a number from 0 to 6 indicating the desired decimial places
+
+  if(val < 0.0){
+    Serial.print('-');
+    val = -val;
+  }
+
+  Serial.print (int(val));  //prints the int part
+  if( precision > 0) {
+    Serial.print("."); // print the decimal point
+    unsigned long frac;
+    unsigned long mult = 1;
+    byte padding = precision -1;
+    while(precision--)
+  mult *=10;
+
+    if(val >= 0)
+ frac = (val - int(val)) * mult;
+    else
+ frac = (int(val)- val ) * mult;
+    unsigned long frac1 = frac;
+    while( frac1 /= 10 )
+ padding--;
+    while(  padding--)
+ Serial.print("0");
+    Serial.print(frac,DEC) ;
+  }
+}
+ 
 
 
 
