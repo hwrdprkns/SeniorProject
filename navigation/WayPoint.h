7b7879dc2493ad5a4bb7dc7070d11bd9257585fd
Taylor Perkins <taylorhp@gmail.com>
2012-03-07 01:59:26 -0500

    Exported WayPoint functions into their own classes for simpler code.

--- navigation/WayPoint.h
+++ navigation/WayPoint.h
@@ -1,4 +1,17 @@
-struct WayPoint{
-  double latitude;
-  double longitude;
+
+#ifndef Waypoint_h
+#define Waypoint_h
+
+#include <Arduino.h>
+
+class WayPoint{
+  
+  public:
+  
+    static double computeDistanceAndBearing(double lat1, double lon1,double lat2, double lon2);
+    static double computeInitialBearing(double lat1, double lon1,double lat2, double lon2);
+    static double computeFinalBearing(double lat1, double lon1,double lat2, double lon2);
+    
 };
+
+#endif
