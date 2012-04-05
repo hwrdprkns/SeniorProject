a7b49dabde2aeef23ebf14f000b547b545ae30db
zhengwy888 <edward.zhengwy@gmail.com>
2012-04-04 22:54:59 -0400

    added init_s2ip. fixed code structure.

--- ATCommand/Command.cpp
+++ ATCommand/Command.cpp
@@ -2,17 +2,19 @@
 #include "Command.h"
 
 extern int sequenceNumber;
+extern int debug;
 
 Command::Command()
 {
   at = "";
   command = "";
+  
 }
 
 String Command::sendComwdg()
 {
   at = "AT*COMWDG=";
-  command = at + sequenceNumber + "\r";
+  command = at + sequenceNumber + "\r\n";
   sequenceNumber++;
   return command;
 }
@@ -20,7 +22,7 @@ String Command::sendComwdg()
 String Command::sendFtrim()
 {
   at = "AT*FTRIM=";
-  command = at + sequenceNumber + "\r";
+  command = at + sequenceNumber + "\r\n";
   sequenceNumber++;
   return command;
 }
@@ -28,7 +30,7 @@ String Command::sendFtrim()
 String Command::sendConfig(String option, String value)
 {
   at = "AT*CONFIG=";
-  command = at + sequenceNumber + ",\"" + option + "\",\"" + value + "\"\r";
+  command = at + sequenceNumber + ",\"" + option + "\",\"" + value + "\"\r\n";
   sequenceNumber++;
   return command;
 }
@@ -37,10 +39,10 @@ String Command::sendRef(int bit9)
 {
   at = "AT*REF=";
   if(bit9 == 1){
-    command = at + sequenceNumber + ",290718208\r"; //takeoff
+    command = at + sequenceNumber + ",290718208\r\n"; //takeoff
   }
   else if(bit9 == 0){
-    command = at + sequenceNumber + ",290717696\r"; //landing
+    command = at + sequenceNumber + ",290717696\r\n"; //landing
   }
   // emergency -> 290717952
   sequenceNumber++;
@@ -50,7 +52,7 @@ String Command::sendRef(int bit9)
 String Command::sendPcmd(int enable, int roll, int pitch, int gaz, int yaw)
 {
   at = "AT*PCMD=";
-  command = at + sequenceNumber + "," + enable + "," + roll + "," + pitch + "," + gaz + "," + yaw + "\r";
+  command = at + sequenceNumber + "," + enable + "," + roll + "," + pitch + "," + gaz + "," + yaw + "\r\n";
   sequenceNumber++;
   return command;
 }
@@ -58,22 +60,79 @@ String Command::sendPcmd(int enable, int roll, int pitch, int gaz, int yaw)
 String Command::sendAnim(int anim, int time)
 {
   at = "AT*ANIM=";
-  command = at + sequenceNumber + "," + anim + "," + time + "\r";
+  command = at + sequenceNumber + "," + anim + "," + time + "\r\n";
   sequenceNumber++;
   return command;
 }
 
-void Command::start_s2ip()
+String Command::LEDAnim(int duration)
 {
-  while (Serial.available()){
+  at = "AT*LED=";
+  command = at + sequenceNumber + ",2,1073741824," + duration + "\r\n";
+  sequenceNumber++;
+  return command;
+}
+
+int Command::start_s2ip()
+{
+  char temp;
+  delay(20000); //wait for drone to start
+  
+  while (ARsrl.available()){
+    PCsrl.write(ARsrl.read());
   }
   
-  Serial.print("\n");
-  Serial.print("cd data/video/apps\n");
-  Serial.print("./s2ip.arm -l\n"); 
+  if (debug) {
+	PCsrl <<"try start s2ip"<<endl;
+  }
+  ARsrl.print("\r\n");
+  delay(500);
+  ARsrl.print("\r\n");
+  delay(500);
+  ARsrl << "cd ~"<<endl;
+  if (debug) {
+	while (ARsrl.available()){
+		PCsrl.write(ARsrl.read());
+	}
+  }
+  delay(500);
+  ARsrl << "cd data/video/apps/"<<endl;
+  delay(500);
+  ARsrl <<"./s2ip.arm -v"<<endl; 
+  while ( (int) temp != 2) {
+    temp = ARsrl.read();
+    if ( temp == 2 ) {
+      PCsrl << "s2ip is running" <<endl;
+      ARsrl << "bullshit\r\n"; //to fix a delay bug
+      break;
+    }
+    //PCsrl<<"s2ip not running" <<endl;
+  }
+  return 1;
 }
 
 void Command::quit_s2ip()
 {
-  Serial.write(03);
+  ARsrl.println("EXIT");
+  while (ARsrl.available () ) {
+    PCsrl.write( ARsrl.read() ); 
+  }
+}
+
+int Command::init_drone() {
+	ARsrl << sendComwdg();
+	ARsrl << sendFtrim();
+	ARsrl << sendConfig("general:navdata_demo","TRUE");
+	ARsrl << sendConfig("control:altitude_max","2000");
+	ARsrl << sendRef(1);
+	
+		//do some checking??
+}
+
+int Command::drone_hover() {
+	while (!drone_is_hover) {
+		ARsrl<< sendPcmd(1,0,0,0,0);
+			// do some checking
+	}
+
 }
--- ATCommand/Command.h
+++ ATCommand/Command.h
@@ -2,7 +2,10 @@
 #define Command_h
 
 #include "Arduino.h"
+#include "Streaming.h"
 
+#define ARsrl Serial1
+#define PCsrl Serial
 
 class Command {
   public:
@@ -13,14 +16,25 @@ class Command {
     String sendRef(int bit9);
     String sendPcmd(int enable, int roll, int pitch, int gaz, int yaw);
     String sendAnim(int anim, int time);
+	String LEDAnim(int duration);
     //void flightMode();
     //void checkStatus();
     //void checkSequenceNumber();
-    void start_s2ip();
+    int start_s2ip();
     void quit_s2ip();
+	
+	// return 1 if drone is initialized
+	int init_drone();
+	// return 1 if drone is hovering
+	int drone_hover();
+	
+	
   private:
     String at;
     String command;
+    
+    int drone_is_hover;
 };
+
 
 #endif
