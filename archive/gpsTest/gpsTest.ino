9c5bdc806b0137b3ea9e70b40fbd4188f156d50b
Edward Zheng <edward.zhengwy@gmail.com>
2012-05-02 23:58:31 -0400

    some more gps code

--- archive/gpsTest/gpsTest.ino
+++ archive/gpsTest/gpsTest.ino
@@ -4,7 +4,7 @@ TinyGPS gps; //Create instance
 
 void setup()
 {
- Serial1.begin(57600); // Baud rate of our GPS
+ Serial3.begin(57600); // Baud rate of our GPS     
  Serial.begin(57600);
 }
 
@@ -14,14 +14,14 @@ void loop(){
 functions that output what ever we want...ie. Latitude or Longitude. The code is made so that 
 boolean newData tells if something is coming in and update the LAT and LON*/ 
 
- bool newData = true; //To check if new data is coming in!
+ bool newData = false; //To check if new data is coming in!
 // For one second we parse GPS data and report values
- for (unsigned long start = millis(); millis() - start < 1000;)
+ for (unsigned long start = millis(); millis() - start < 2000;)
  {
-   while (Serial1.available())
+   while (Serial3.available())
    {
-     char c = Serial1.read();
-     //Serial.write(c); // Helps to see GPS data flow
+     char c = Serial3.read();
+     Serial.write(c); // Helps to see GPS data flow
      if (gps.encode(c)) // Did a new valid sentence come in?
        newData = true; //
    }
@@ -32,20 +32,22 @@ boolean newData tells if something is coming in and update the LAT and LON*/
    float flat, flon;
    unsigned long age;
    gps.f_get_position(&flat, &flon, &age);
-   /*Serial.print("\nLAT= ");
-   Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+   Serial.print("\nLAT= ");
+   Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 8);
    Serial.print("\nLON= ");
-   Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+   Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 8);
    Serial.print("\nSAT= ");
    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
    Serial.print("\nPREC= ");
    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
    Serial.print("\nAGE= ");
    Serial.print(age);
-   Serial.print("\nCOURSE=");*/
+   Serial.print("\nCOURSE=");
    Serial.print(gps.course());
    Serial.print(",");
-   //Serial.print("\n");
+   Serial.print("\nf_course=");
+   Serial.print(gps.f_course());
+   Serial.print("\n");
  }
 
 }
